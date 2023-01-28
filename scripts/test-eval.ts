9c756e6e0e41b87843f81a78ee2ac3cb13deed19
Patrik Dvořáček <patrik@dvoracek.dev>
2023-01-28 15:32:12 +0100

    feat: :sparkles: Convert scripts/test-eval.js to TypeScript.
    Additionally, add types for jsdom.

---
+++ scripts/test-eval.ts
@@ -0,0 +1,40 @@
+/**
+ * Runs all surrogates inside of a (fake) browser enviroment to check for obvious runtime issues.
+ */
+import fs from 'fs';
+import path from 'path';
+import jsdom from 'jsdom';
+import { exit } from 'process';
+
+const surrogatesDir = path.join(__dirname, '../surrogates');
+
+const allSurrogates = fs.readdirSync(surrogatesDir).map(filename => {
+    const filepath = path.resolve(surrogatesDir, filename);
+    const stat = fs.statSync(filepath);
+
+    if (!stat.isFile()) {
+        return Promise.resolve();
+    }
+
+    const surrogateText = fs.readFileSync(filepath, 'utf-8');
+    const virtualConsole = new jsdom.VirtualConsole();
+
+    let resolve: (value?: unknown) => void;
+    let reject: (reason: string) => void;
+    const promise = new Promise((resolveCallback, rejectCallback) => { resolve = resolveCallback; reject = rejectCallback; });
+
+    virtualConsole.on('jsdomError', (e) => {
+        reject(`🛑 ${filename} fails with error "${e}"`);
+    });
+
+    const dom = new jsdom.JSDOM(`<body><script>${surrogateText}</script></body>`, { runScripts: 'dangerously', virtualConsole });
+    dom.window.addEventListener('DOMContentLoaded', () => resolve());
+
+    return promise;
+});
+
+Promise.all(allSurrogates)
+    .catch(e => {
+        console.log(e);
+        exit(1);
+    });
