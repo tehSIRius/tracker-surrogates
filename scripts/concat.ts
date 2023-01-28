2c6965e9fc80be8d008b1ddf4564aff7d3f226f4
Patrik Dvořáček <patrik@dvoracek.dev>
2023-01-28 19:29:17 +0100

    feat: :sparkles: Add bundling with esbuild to concat.ts.
    This change should allow for advanced features and TypeScript usage in the surrogates.

--- scripts/concat.ts
+++ scripts/concat.ts
@@ -1,22 +1,18 @@
-/**
- * Builds single file with all surrogates consumed by clients that fetch surrogates from a remote endpoint.
- */
 import fs from 'fs';
 import path from 'path';
 import crypto from 'crypto';
 import untypedMapping from '../mapping.json';
+import { buildSync } from 'esbuild';
 
 interface SurrogateRule {
-    regexRule: string
-    surrogate: string
+	regexRule: string;
+	surrogate: string;
 }
 // The retyping here is not the cleanest but it is the easiest way to get it working.
 const mapping: Record<string, SurrogateRule[]> = untypedMapping;
 
 const surrogatesDir = path.join(path.dirname(__dirname), 'surrogates');
-const buildsDir = path.join(path.dirname(__dirname), 'builds');
-
-const surrogates = fs.readdirSync(surrogatesDir).map((surrogate: string) => {
+const surrogates = fs.readdirSync(surrogatesDir).map((surrogate) => {
     const filepath = path.resolve(surrogatesDir, surrogate);
     const stat = fs.statSync(filepath);
 
@@ -24,8 +20,6 @@ const surrogates = fs.readdirSync(surrogatesDir).map((surrogate: string) => {
         return '';
     }
 
-    // strip blank lines from surrogates, or it breaks parsing in BSK
-    const text = fs.readFileSync(filepath, 'utf-8').replace(/^\s*[\r\n]/gm, '');
     const domain = Object.keys(mapping).find((domainKey) =>
         mapping[domainKey].find((s) => s.surrogate === surrogate)
     );
@@ -34,18 +28,32 @@ const surrogates = fs.readdirSync(surrogatesDir).map((surrogate: string) => {
         throw new Error(`🛑 Domain for surrogate missing - ${surrogate}.`);
     }
 
-    return `${domain}/${surrogate} application/javascript\n${text}`;
+    const content = buildSync({
+        stdin: { contents: '' },
+        inject: [filepath],
+        write: false,
+        outdir: 'out',
+    });
+
+    if (content.errors.length) {
+        throw new Error(
+            `🛑 Could not compile surrogate ${surrogate} - ${content.errors}`
+        );
+    }
+
+    return `${domain}/${surrogate} application/javascript\n${content.outputFiles[0].text}`;
 });
 
-let output = `# This file contains "surrogates". Surrogates are small scripts that our apps and extensions serve in place of trackers that cause site breakage when blocked.
+const introduction = `# This file contains "surrogates". Surrogates are small scripts that our apps and extensions serve in place of trackers that cause site breakage when blocked.
 # Learn more: https://github.com/duckduckgo/tracker-surrogates`;
-output = [output, ...surrogates].join('\n');
 
+const buildsDir = path.join(path.dirname(__dirname), 'builds');
 if (!fs.existsSync(buildsDir)) {
     fs.mkdirSync(buildsDir);
 }
 
+const output = [introduction, ...surrogates].join('\n');
 fs.writeFileSync(path.join(buildsDir, 'surrogates-next.txt'), output);
 
 const hash = crypto.createHash('md5').update(output).digest('hex');
-console.log('MD5 hash: ', hash);
+console.log(`MD5 hash: `, hash);
